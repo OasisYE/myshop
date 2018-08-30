a6e77688647cdba6f945e8da7b0c5adfb62eb3c9
“OasisYE” <YWJwejan@126.com>
2018-08-30 23:41:17 +0800

    fix

--- myobject/settings.py
+++ myobject/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shopdb',
         'USER': 'root',
-        'PASSWORD': '12345678.a',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
